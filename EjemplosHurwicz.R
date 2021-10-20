c370ba1a63c0de6898dfc79375ad7e4d8a249dbc
anasolgar <81254292+anasolgar@users.noreply.github.com>
2021-10-20 19:37:02 +0200

    Update EjemplosHurwicz.R

--- EjemplosHurwicz.R
+++ EjemplosHurwicz.R
@@ -1,1 +1,23 @@
 # Enunciados de los ejercicios para aplicar el Método de Hurwicz
+
+
+# Ejercicio 1 (Ana)
+
+# Una familia tiene un local al que le quiere dar uso. Una de las hijas de la familia, María, decide que quiere montar un negocio y
+# está estudiando qué opción le sale más rentable en cuanto a beneficios (al mes). Se plantea montar en el local una tienda de animales,
+# una floristería, una tienda de juguetes o una pastelería. Los beneficios van en función de si va a montar el negocio sola (e1), 
+# con su hermano (e2) o con sus primos (e3). 
+# 
+# - Si montara una tienda de animales ella sola, finalmente se llevaría 200 euros de beneficio, mientras que si la monta con su hermano, 
+# debido a gastos de otra empresa común, el beneficio sería de 180 euros. Con los primos podría ganar 230 euros.
+# 
+# - Con floristería para ella sola supondría ganar 110 euros al mes. Si la montara con el hermano se llevaría de beneficio 50 euros, de nuevo
+# a causa de la otra empresa común y si fuese con sus primos se llevaría 100.
+# 
+# - Si decidiese montar la tienda de juguetes sola, ganaría 150 euros al mes. Como el resto de la familia también lleva establecimientos
+# relacionados con el ocio de los niños, al final si decidiese hacerlo con su hermano ganaría 240 euros y con sus primos 200.
+# 
+# - Si pusiese una pastelería con sus primos, no tendría que pagar tanto dinero a los empleados porque sus primos, al ser pasteleros, 
+# trabajarían allí y finalmente, se llevaría 270 euros al mes. Si la montase con su hermano serían unos 150 euros y sola 190.
+# 
+# ¿Con qué opción ganaría María más dinero al mes? 
